53c36a97f40760b9b5df38e2c652db68913cecf4
Jayesh Patil <147319090+jayesh2811@users.noreply.github.com>
2025-04-30 12:51:35 +0530

    Update hooks.py

--- hr_customization/hooks.py
+++ hr_customization/hooks.py
@@ -5,240 +5,240 @@ app_description = "Custom HR App"
 app_email = "jayesh.patil@techonsy.com"
 app_license = "mit"
 
-# Apps
-# ------------------
+Apps
+------------------
 
-# required_apps = []
+required_apps = []
 
-# Each item in the list will be shown as an app in the apps page
-# add_to_apps_screen = [
-# 	{
-# 		"name": "hr_customization",
-# 		"logo": "/assets/hr_customization/logo.png",
-# 		"title": "HR Customization",
-# 		"route": "/hr_customization",
-# 		"has_permission": "hr_customization.api.permission.has_app_permission"
-# 	}
-# ]
+Each item in the list will be shown as an app in the apps page
+add_to_apps_screen = [
+	{
+		"name": "hr_customization",
+		"logo": "/assets/hr_customization/logo.png",
+		"title": "HR Customization",
+		"route": "/hr_customization",
+		"has_permission": "hr_customization.api.permission.has_app_permission"
+	}
+]
 
-# Includes in <head>
-# ------------------
+Includes in <head>
+------------------
 
-# include js, css files in header of desk.html
-# app_include_css = "/assets/hr_customization/css/hr_customization.css"
-# app_include_js = "/assets/hr_customization/js/hr_customization.js"
+include js, css files in header of desk.html
+app_include_css = "/assets/hr_customization/css/hr_customization.css"
+app_include_js = "/assets/hr_customization/js/hr_customization.js"
 
-# include js, css files in header of web template
-# web_include_css = "/assets/hr_customization/css/hr_customization.css"
-# web_include_js = "/assets/hr_customization/js/hr_customization.js"
+include js, css files in header of web template
+web_include_css = "/assets/hr_customization/css/hr_customization.css"
+web_include_js = "/assets/hr_customization/js/hr_customization.js"
 
-# include custom scss in every website theme (without file extension ".scss")
-# website_theme_scss = "hr_customization/public/scss/website"
+include custom scss in every website theme (without file extension ".scss")
+website_theme_scss = "hr_customization/public/scss/website"
 
-# include js, css files in header of web form
-# webform_include_js = {"doctype": "public/js/doctype.js"}
-# webform_include_css = {"doctype": "public/css/doctype.css"}
+include js, css files in header of web form
+webform_include_js = {"doctype": "public/js/doctype.js"}
+webform_include_css = {"doctype": "public/css/doctype.css"}
 
-# include js in page
-# page_js = {"page" : "public/js/file.js"}
+include js in page
+page_js = {"page" : "public/js/file.js"}
 
-# include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
-# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
-# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
-# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
+include js in doctype views
+doctype_js = {"doctype" : "public/js/doctype.js"}
+doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
+doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
-# Svg Icons
-# ------------------
-# include app icons in desk
-# app_include_icons = "hr_customization/public/icons.svg"
+Svg Icons
+------------------
+include app icons in desk
+app_include_icons = "hr_customization/public/icons.svg"
 
-# Home Pages
-# ----------
+Home Pages
+----------
 
-# application home page (will override Website Settings)
-# home_page = "login"
+application home page (will override Website Settings)
+home_page = "login"
 
-# website user home page (by Role)
-# role_home_page = {
-# 	"Role": "home_page"
-# }
+website user home page (by Role)
+role_home_page = {
+	"Role": "home_page"
+}
 
-# Generators
-# ----------
+Generators
+----------
 
-# automatically create page for each record of this doctype
-# website_generators = ["Web Page"]
+automatically create page for each record of this doctype
+website_generators = ["Web Page"]
 
-# Jinja
-# ----------
+Jinja
+----------
 
-# add methods and filters to jinja environment
-# jinja = {
-# 	"methods": "hr_customization.utils.jinja_methods",
-# 	"filters": "hr_customization.utils.jinja_filters"
-# }
+add methods and filters to jinja environment
+jinja = {
+	"methods": "hr_customization.utils.jinja_methods",
+	"filters": "hr_customization.utils.jinja_filters"
+}
 
-# Installation
-# ------------
+Installation
+------------
 
-# before_install = "hr_customization.install.before_install"
-# after_install = "hr_customization.install.after_install"
+before_install = "hr_customization.install.before_install"
+after_install = "hr_customization.install.after_install"
 
-# Uninstallation
-# ------------
+Uninstallation
+------------
 
-# before_uninstall = "hr_customization.uninstall.before_uninstall"
-# after_uninstall = "hr_customization.uninstall.after_uninstall"
+before_uninstall = "hr_customization.uninstall.before_uninstall"
+after_uninstall = "hr_customization.uninstall.after_uninstall"
 
-# Integration Setup
-# ------------------
-# To set up dependencies/integrations with other apps
-# Name of the app being installed is passed as an argument
+Integration Setup
+------------------
+To set up dependencies/integrations with other apps
+Name of the app being installed is passed as an argument
 
-# before_app_install = "hr_customization.utils.before_app_install"
-# after_app_install = "hr_customization.utils.after_app_install"
+before_app_install = "hr_customization.utils.before_app_install"
+after_app_install = "hr_customization.utils.after_app_install"
 
-# Integration Cleanup
-# -------------------
-# To clean up dependencies/integrations with other apps
-# Name of the app being uninstalled is passed as an argument
+Integration Cleanup
+-------------------
+To clean up dependencies/integrations with other apps
+Name of the app being uninstalled is passed as an argument
 
-# before_app_uninstall = "hr_customization.utils.before_app_uninstall"
-# after_app_uninstall = "hr_customization.utils.after_app_uninstall"
+before_app_uninstall = "hr_customization.utils.before_app_uninstall"
+after_app_uninstall = "hr_customization.utils.after_app_uninstall"
 
-# Desk Notifications
-# ------------------
-# See frappe.core.notifications.get_notification_config
+Desk Notifications
+------------------
+See frappe.core.notifications.get_notification_config
 
-# notification_config = "hr_customization.notifications.get_notification_config"
+notification_config = "hr_customization.notifications.get_notification_config"
 
-# Permissions
-# -----------
-# Permissions evaluated in scripted ways
+Permissions
+-----------
+Permissions evaluated in scripted ways
 
-# permission_query_conditions = {
-# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
-# }
-#
-# has_permission = {
-# 	"Event": "frappe.desk.doctype.event.event.has_permission",
-# }
+permission_query_conditions = {
+	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+}
 
-# DocType Class
-# ---------------
-# Override standard doctype classes
+has_permission = {
+	"Event": "frappe.desk.doctype.event.event.has_permission",
+}
 
-# override_doctype_class = {
-# 	"ToDo": "custom_app.overrides.CustomToDo"
-# }
+DocType Class
+---------------
+Override standard doctype classes
 
-# Document Events
-# ---------------
-# Hook on document methods and events
+override_doctype_class = {
+	"ToDo": "custom_app.overrides.CustomToDo"
+}
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-# 	}
-# }
+Document Events
+---------------
+Hook on document methods and events
 
-# Scheduled Tasks
-# ---------------
+doc_events = {
+	"*": {
+		"on_update": "method",
+		"on_cancel": "method",
+		"on_trash": "method"
+	}
+}
 
-# scheduler_events = {
-# 	"all": [
-# 		"hr_customization.tasks.all"
-# 	],
-# 	"daily": [
-# 		"hr_customization.tasks.daily"
-# 	],
-# 	"hourly": [
-# 		"hr_customization.tasks.hourly"
-# 	],
-# 	"weekly": [
-# 		"hr_customization.tasks.weekly"
-# 	],
-# 	"monthly": [
-# 		"hr_customization.tasks.monthly"
-# 	],
-# }
+Scheduled Tasks
+---------------
 
-# Testing
-# -------
+scheduler_events = {
+	"all": [
+		"hr_customization.tasks.all"
+	],
+	"daily": [
+		"hr_customization.tasks.daily"
+	],
+	"hourly": [
+		"hr_customization.tasks.hourly"
+	],
+	"weekly": [
+		"hr_customization.tasks.weekly"
+	],
+	"monthly": [
+		"hr_customization.tasks.monthly"
+	],
+}
 
-# before_tests = "hr_customization.install.before_tests"
+Testing
+-------
 
-# Overriding Methods
-# ------------------------------
-#
-# override_whitelisted_methods = {
-# 	"frappe.desk.doctype.event.event.get_events": "hr_customization.event.get_events"
-# }
-#
-# each overriding function accepts a `data` argument;
-# generated from the base implementation of the doctype dashboard,
-# along with any modifications made in other Frappe apps
-# override_doctype_dashboards = {
-# 	"Task": "hr_customization.task.get_dashboard_data"
-# }
+before_tests = "hr_customization.install.before_tests"
 
-# exempt linked doctypes from being automatically cancelled
-#
-# auto_cancel_exempted_doctypes = ["Auto Repeat"]
+Overriding Methods
+------------------------------
 
-# Ignore links to specified DocTypes when deleting documents
-# -----------------------------------------------------------
+override_whitelisted_methods = {
+	"frappe.desk.doctype.event.event.get_events": "hr_customization.event.get_events"
+}
 
-# ignore_links_on_delete = ["Communication", "ToDo"]
+each overriding function accepts a `data` argument;
+generated from the base implementation of the doctype dashboard,
+along with any modifications made in other Frappe apps
+override_doctype_dashboards = {
+	"Task": "hr_customization.task.get_dashboard_data"
+}
 
-# Request Events
-# ----------------
-# before_request = ["hr_customization.utils.before_request"]
-# after_request = ["hr_customization.utils.after_request"]
+exempt linked doctypes from being automatically cancelled
 
-# Job Events
-# ----------
-# before_job = ["hr_customization.utils.before_job"]
-# after_job = ["hr_customization.utils.after_job"]
+auto_cancel_exempted_doctypes = ["Auto Repeat"]
 
-# User Data Protection
-# --------------------
+Ignore links to specified DocTypes when deleting documents
+-----------------------------------------------------------
 
-# user_data_fields = [
-# 	{
-# 		"doctype": "{doctype_1}",
-# 		"filter_by": "{filter_by}",
-# 		"redact_fields": ["{field_1}", "{field_2}"],
-# 		"partial": 1,
-# 	},
-# 	{
-# 		"doctype": "{doctype_2}",
-# 		"filter_by": "{filter_by}",
-# 		"partial": 1,
-# 	},
-# 	{
-# 		"doctype": "{doctype_3}",
-# 		"strict": False,
-# 	},
-# 	{
-# 		"doctype": "{doctype_4}"
-# 	}
-# ]
+ignore_links_on_delete = ["Communication", "ToDo"]
 
-# Authentication and authorization
-# --------------------------------
+Request Events
+----------------
+before_request = ["hr_customization.utils.before_request"]
+after_request = ["hr_customization.utils.after_request"]
 
-# auth_hooks = [
-# 	"hr_customization.auth.validate"
-# ]
+Job Events
+----------
+before_job = ["hr_customization.utils.before_job"]
+after_job = ["hr_customization.utils.after_job"]
 
-# Automatically update python controller files with type annotations for this app.
-# export_python_type_annotations = True
+User Data Protection
+--------------------
 
-# default_log_clearing_doctypes = {
-# 	"Logging DocType Name": 30  # days to retain logs
-# }
+user_data_fields = [
+	{
+		"doctype": "{doctype_1}",
+		"filter_by": "{filter_by}",
+		"redact_fields": ["{field_1}", "{field_2}"],
+		"partial": 1,
+	},
+	{
+		"doctype": "{doctype_2}",
+		"filter_by": "{filter_by}",
+		"partial": 1,
+	},
+	{
+		"doctype": "{doctype_3}",
+		"strict": False,
+	},
+	{
+		"doctype": "{doctype_4}"
+	}
+]
+
+Authentication and authorization
+--------------------------------
+
+auth_hooks = [
+	"hr_customization.auth.validate"
+]
+
+Automatically update python controller files with type annotations for this app.
+export_python_type_annotations = True
+
+default_log_clearing_doctypes = {
+	"Logging DocType Name": 30  # days to retain logs
+}
 
