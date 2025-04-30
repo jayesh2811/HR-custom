9c80326657b07cbaa9bb3109b31560f0b349acff
Jayesh Patil <147319090+jayesh2811@users.noreply.github.com>
2025-04-30 14:52:50 +0530

    Create install.py

---
+++ hr_customization/hr_customization/install.py
@@ -0,0 +1,10 @@
+# apps/hr_customization/hr_customization/install.py
+
+def before_install():
+    print("Running before_install hook for hr_customization")
+
+def after_install():
+    print("Running after_install hook for hr_customization")
+
+def before_tests():
+    print("Running before_tests hook for hr_customization")
